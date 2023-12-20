3d9a0969b9d716cfce19a47227a3827b9d69e452
fiticosmica <154455286+fiticosmica@users.noreply.github.com>
2023-12-20 13:48:39 -0300

    Update sitio.py

--- sitio.py
+++ sitio.py
@@ -1,12 +1,11 @@
 import streamlit as st 
 import requests 
-from PIL import Image 
 
 lottie_coding = load_lottieurl("https://lottie.host/622dd294-8daf-4b01-b724-2afe598896f2/OpsEJSLUwN.json")
-img_ctci = Image.open("/content/Marca-CTCI.png")
+#img_ctci = Image.open("/content/Marca-CTCI.png")
 
 with st.container():
-  st.image(img_ctci, width=200)
+  #st.image(img_ctci, width=200)
   st.title("CTCI Nodo centro sur")
   st.write("El objetivo general es co-crear un modelo de Ciencia Abierta para fortalecer el desarrollo de la ciencia y tecnología en la Macrozona Centro Sur de Chile en concordancia con su territorio y sociedad.")
   st.write("[Más información >](https://www.ctci.cl/)") #vinculo web
